56b4b476c4f423a1f870053ecfded5372825dd9a
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-11-05 21:38:20 -0200

    update project

---
+++ src/main/java/com/javaee/bruno/springmvc/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package com.javaee.bruno.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javaee.bruno.springmvc.services.BookService;
+
+@Controller
+public class IndexController {
+	
+	private final BookService bookService;
+	
+	public IndexController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@RequestMapping({"","/","/index"})
+	public String getIndexPage(Model model) {
+		model.addAttribute("books",bookService.getBooks());
+		 return "index";
+	}
+
+}
---
+++ src/main/java/com/javaee/bruno/springmvc/domain/Book.java
@@ -0,0 +1,15 @@
+package com.javaee.bruno.springmvc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+
+	private Long id;
+	
+	private String name;
+	
+	private String description;
+}
